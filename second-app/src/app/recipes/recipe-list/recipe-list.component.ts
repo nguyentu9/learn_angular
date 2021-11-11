f0a1dd001c7866b15a42a3720349fa9da7b210fc
nguyentu9 <huynhthanhtoan517@gmail.com>
2021-11-11 19:55:01 +0700

    3. 10 Adding Content to the Recipes Components & 3. 11 Outputting a List of Recipes with ngFor

--- second-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ second-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipes.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe',
+      'This is simply a test',
+      'https://static.onecms.io/wp-content/uploads/sites/44/2020/03/03/7782449.jpg'
+    ),
+    new Recipe(
+      'A Test Recipe',
+      'This is simply a test',
+      'https://static.onecms.io/wp-content/uploads/sites/44/2020/03/03/7782449.jpg'
+    ),
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
