9881bc40654784d4d3dace5eddc7f364273ac89d
nguyentu9 <huynhthanhtoan517@gmail.com>
2021-11-21 08:06:17 +0700

    5. 6 Binding to Custom Events

--- tertiary-app/src/app/app.component.ts
+++ tertiary-app/src/app/app.component.ts
@@ -9,4 +9,19 @@ export class AppComponent {
   serverElements = [
     { type: 'server', name: 'Testserver', content: 'Just an examples' },
   ];
+  onServerAdded(serverData: { serverName: string; serverContent: string }) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent,
+    });
+  }
+
+  onBluePrintAdded(serverData: { serverName: string; serverContent: string }) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: serverData.serverName,
+      content: serverData.serverContent,
+    });
+  }
 }
