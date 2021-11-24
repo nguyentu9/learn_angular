c28aa6f8b59bfb8f831326283bde08727da58dd6
nguyentu9 <huynhthanhtoan517@gmail.com>
2021-11-24 14:43:23 +0700

    6. 6 Allowing the User to Add Ingredients to the Shopping List

--- second-app/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ second-app/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  OnInit,
+  Output,
+  ViewChild,
+} from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
-  styleUrls: ['./shopping-edit.component.css']
+  styleUrls: ['./shopping-edit.component.css'],
 })
 export class ShoppingEditComponent implements OnInit {
+  @ViewChild('nameInput', { static: false })
+  nameInputRef!: ElementRef;
 
-  constructor() { }
+  @ViewChild('amountInput', { static: false })
+  amountInputRef!: ElementRef;
 
-  ngOnInit(): void {
+  @Output()
+  ingredientAdded = new EventEmitter<Ingredient>();
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  addItem() {
+    const ingName = this.nameInputRef.nativeElement.value;
+    const ingAmount = this.amountInputRef.nativeElement.value;
+    const newIngredient = new Ingredient(ingName, ingAmount);
+
+    this.ingredientAdded.emit(newIngredient);
   }
-
 }
