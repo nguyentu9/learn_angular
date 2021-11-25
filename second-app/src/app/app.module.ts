7794a14ff846418a94ae6d01f836c5d9d7104ce7
nguyentu9 <huynhthanhtoan517@gmail.com>
2021-11-25 09:11:23 +0700

    8. 1 Building and Using a Dropdown Directive

--- second-app/src/app/app.module.ts
+++ second-app/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { HeaderComponent } from './header/header.component';
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { DropdownDirective } from './shared/dropdown.directive';
 
 @NgModule({
   declarations: [
@@ -20,6 +21,7 @@ import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-it
     ShoppingListComponent,
     ShoppingEditComponent,
     RecipeItemComponent,
+    DropdownDirective,
   ],
   imports: [BrowserModule],
   providers: [],
---
+++ second-app/src/app/shared/dropdown.directive.ts
@@ -0,0 +1,12 @@
+import { Directive, HostBinding, HostListener } from '@angular/core';
+
+@Directive({
+  selector: '[appDropdown]',
+})
+export class DropdownDirective {
+  @HostBinding('class.open') isOpen: boolean = false;
+
+  @HostListener('click') onclick() {
+    this.isOpen = !this.isOpen;
+  }
+}
