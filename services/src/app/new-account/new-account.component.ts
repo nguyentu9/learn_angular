0968d98908e0671b12e38696e942a144eb69fca4
nguyentu9 <huynhthanhtoan517@gmail.com>
2021-11-28 23:01:44 +0700

    9. 7 How many Instances of Service Should It Be

--- services/src/app/new-account/new-account.component.ts
+++ services/src/app/new-account/new-account.component.ts
@@ -1,11 +1,11 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component } from '@angular/core';
 import { AccountsService } from '../accounts.service';
 import { LoggingService } from '../logging.service';
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
   styleUrls: ['./new-account.component.css'],
-  providers: [LoggingService, AccountsService],
+  providers: [LoggingService],
 })
 export class NewAccountComponent {
   constructor(
