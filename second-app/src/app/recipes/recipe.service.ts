b94aa5dd5abf6b618c02054d960ef2f69ac85602
nguyentu9 <huynhthanhtoan517@gmail.com>
2021-12-01 07:56:57 +0700

    10. 7 Adding Ingredients to Recipes

--- second-app/src/app/recipes/recipe.service.ts
+++ second-app/src/app/recipes/recipe.service.ts
@@ -1,17 +1,20 @@
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 import { Recipe } from './recipes.model';
 export class RecipeService {
   public recipeSelected = new EventEmitter<Recipe>();
   private recipes: Recipe[] = [
     new Recipe(
-      'A Test Recipe',
-      'This is simply a test',
-      'https://static.onecms.io/wp-content/uploads/sites/44/2020/03/03/7782449.jpg'
+      'Tasty Schnitzel',
+      'A super-tasty Schnitzel - just awesome!',
+      'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG',
+      [new Ingredient('Meat', 1), new Ingredient('French Fries', 20)]
     ),
     new Recipe(
-      'Another Test Recipe',
-      'This is simply a test',
-      'https://static.onecms.io/wp-content/uploads/sites/44/2020/03/03/7782449.jpg'
+      'Big Fat Burger',
+      'What else you need to say?',
+      'https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg',
+      [new Ingredient('Buns', 2), new Ingredient('Meat', 1)]
     ),
   ];
 
