b59fbcd25288212099fd97aab9a702c50ef404e1
nguyentu9 <huynhthanhtoan517@gmail.com>
2021-12-01 23:24:20 +0700

    17. Setting up Child (Nested) Routes

--- routing/src/app/servers/server/server.component.ts
+++ routing/src/app/servers/server/server.component.ts
@@ -20,8 +20,8 @@ export class ServerComponent implements OnInit {
     const id = +this.route.snapshot.params['id'];
     this.server = this.serversService.getServer(id);
 
-    // this.route.params.subscribe((params: Params) => {
-    //   this.server = this.serversService.getServer(params['id']);
-    // });
+    this.route.params.subscribe((params: Params) => {
+      this.server = this.serversService.getServer(+params['id']);
+    });
   }
 }
