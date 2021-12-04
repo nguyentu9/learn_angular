fb38818f3e84fa3e6dd0bd49e227b51db0fd37e0
nguyentu9 <huynhthanhtoan517@gmail.com>
2021-12-05 04:51:02 +0700

    16. 9 Allowing the Deletion of Shopping List Items

--- second-app/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ second-app/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -62,6 +62,12 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
     this.editMode = false;
   }
 
+  onDelete() {
+    this.shoppingListService.deleteIngredient(this.editedItemIndex);
+    console.log(this.editedItemIndex);
+    this.onClear();
+  }
+
   ngOnDestroy() {
     this.subscription.unsubscribe();
   }
