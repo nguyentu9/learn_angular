dff41ef618fd96dcafe9f464067e3b06c3c5dc9a
nguyentu9 <huynhthanhtoan517@gmail.com>
2021-12-04 15:11:53 +0700

    15. 18 TD Using Form Data

--- forms/src/app/app.component.ts
+++ forms/src/app/app.component.ts
@@ -11,6 +11,15 @@ export class AppComponent {
   defaultQuestion = 'pet';
   answer = '';
   genders = ['male', 'female'];
+  submitted = false;
+
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: '',
+  };
 
   suggestUserName() {
     const suggestedName = 'Superuser';
@@ -40,6 +49,12 @@ export class AppComponent {
   //   }
 
   onSubmit() {
-    console.log(this.signupForm);
+    // console.log(this.signupForm);
+    this.submitted = true;
+    this.user.username = this.signupForm.value.userData.username;
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.secretQuestion = this.signupForm.value.secret;
+    this.user.answer = this.signupForm.value.questionAnswer;
+    this.user.gender = this.signupForm.value.gender;
   }
 }
