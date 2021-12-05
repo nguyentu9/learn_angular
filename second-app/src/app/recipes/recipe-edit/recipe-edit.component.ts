8ef4e9f60e139494caf9454479372378a3d00d82
nguyentu9 <huynhthanhtoan517@gmail.com>
2021-12-05 11:14:50 +0700

    16. 16 Validating User Input

--- second-app/src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ second-app/src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -29,8 +29,11 @@ export class RecipeEditComponent implements OnInit {
   addIngredient() {
     (<FormArray>this.recipeForm.get('ingredients')).push(
       new FormGroup({
-        name: new FormControl(),
-        amount: new FormControl(),
+        name: new FormControl(null, Validators.required),
+        amount: new FormControl(null, [
+          Validators.required,
+          Validators.pattern(/^[1-9]+[0-9]*$/),
+        ]),
       })
     );
   }
@@ -52,8 +55,11 @@ export class RecipeEditComponent implements OnInit {
         for (let ingredient of recipe.ingredients) {
           recipeIngredients.push(
             new FormGroup({
-              name: new FormControl(ingredient.name),
-              amount: new FormControl(ingredient.amount),
+              name: new FormControl(ingredient.name, Validators.required),
+              amount: new FormControl(ingredient.amount, [
+                Validators.required,
+                Validators.pattern(/^[1-9]+[0-9]*$/),
+              ]),
             })
           );
         }
