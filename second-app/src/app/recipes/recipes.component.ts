c04c8db3ae98293b7ad98447b30d3688815f44c1
nguyentu9 <huynhthanhtoan517@gmail.com>
2021-12-05 11:57:39 +0700

    16. 21 Providing the Recipe Service Correctly

--- second-app/src/app/recipes/recipes.component.ts
+++ second-app/src/app/recipes/recipes.component.ts
@@ -6,7 +6,6 @@ import { Recipe } from './recipes.model';
   selector: 'app-recipes',
   templateUrl: './recipes.component.html',
   styleUrls: ['./recipes.component.css'],
-  providers: [RecipeService],
 })
 export class RecipesComponent implements OnInit {
   selectedRecipe!: Recipe;
