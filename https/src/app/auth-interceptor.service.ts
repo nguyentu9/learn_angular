334ffe6849e5d6b159def1a19bc8ac52c8a9c4d9
nguyentu9 <huynhthanhtoan517@gmail.com>
2021-12-08 02:45:15 +0700

    25 Response Interceptors

--- https/src/app/auth-interceptor.service.ts
+++ https/src/app/auth-interceptor.service.ts
@@ -1,8 +1,10 @@
 import {
+  HttpEventType,
   HttpHandler,
   HttpInterceptor,
   HttpRequest,
 } from '@angular/common/http';
+import { tap } from 'rxjs/operators';
 
 export class AuthInterceptorService implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler) {
@@ -12,6 +14,14 @@ export class AuthInterceptorService implements HttpInterceptor {
       headers: req.headers.append('Auth', 'xyz'),
       // url: 'some-new-url'
     });
-    return next.handle(modifiedRequest);
+    return next.handle(modifiedRequest).pipe(
+      tap((event) => {
+        console.log(event);
+        if (event.type === HttpEventType.Response) {
+          console.log('Response arrived, body data: ');
+          console.log(event.body);
+        }
+      })
+    );
   }
 }
