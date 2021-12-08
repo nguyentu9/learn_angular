d8dde03e4198db83eaff221aa92dd65c179036a3
nguyentu9 <huynhthanhtoan517@gmail.com>
2021-12-08 14:04:41 +0700

    19. 4 Storing Recipes

--- second-app/src/app/shared/data-storage.service.ts
+++ second-app/src/app/shared/data-storage.service.ts
@@ -1,7 +1,15 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { RecipeService } from '../recipes/recipe.service';
 
 @Injectable({ providedIn: 'root' })
 export class DataStorageService {
-  constructor(private http: HttpClient) {}
+  private apiURL: string =
+    'https://ng-course-recipe-book-63a85-default-rtdb.asia-southeast1.firebasedatabase.app/';
+  constructor(private http: HttpClient, private recipeService: RecipeService) {}
+
+  storeRecipes() {
+    const recipes = this.recipeService.getRecipes();
+    this.http.put(this.apiURL, recipes).subscribe((res) => console.log(res));
+  }
 }
