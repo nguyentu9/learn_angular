fae31be553b6a73bbd223144179005ad41a586ba
nguyentu9 <huynhthanhtoan517@gmail.com>
2022-02-23 12:19:51 +0700

    19. 8 Fixing a Bug with the Resolver

---
+++ second-app/src/app/recipes/recipes-resolver.service.ts
@@ -0,0 +1,26 @@
+import { Recipe } from './recipes.model';
+import {
+  ActivatedRouteSnapshot,
+  Resolve,
+  RouterStateSnapshot,
+} from '@angular/router';
+import { Injectable } from '@angular/core';
+import { DataStorageService } from '../shared/data-storage.service';
+import { RecipeService } from './recipe.service';
+
+@Injectable({ providedIn: 'root' })
+export class RecipesResolverService implements Resolve<Recipe[]> {
+  constructor(
+    private dataStorageService: DataStorageService,
+    private recipesService: RecipeService
+  ) {}
+
+  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    const recipes = this.recipesService.getRecipes();
+    if (recipes.length === 0) {
+      return this.dataStorageService.fetchRecipes();
+    } else {
+      return recipes;
+    }
+  }
+}
--- second-app/src/app/shared/data-storage.service.ts
+++ second-app/src/app/shared/data-storage.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { map } from 'rxjs';
+import { map, tap } from 'rxjs';
 import { RecipeService } from '../recipes/recipe.service';
 import { Recipe } from '../recipes/recipes.model';
 
@@ -16,20 +16,18 @@ export class DataStorageService {
   }
 
   fetchRecipes() {
-    this.http
-      .get<Recipe[]>(this.apiURL)
-      .pipe(
-        map((recipes) => {
-          return recipes.map((recipe) => {
-            return {
-              ...recipe,
-              ingredients: recipe.ingredients ? recipe.ingredients : [],
-            };
-          });
-        })
-      )
-      .subscribe((recipes) => {
+    return this.http.get<Recipe[]>(this.apiURL).pipe(
+      map((recipes) => {
+        return recipes.map((recipe) => {
+          return {
+            ...recipe,
+            ingredients: recipe.ingredients ? recipe.ingredients : [],
+          };
+        });
+      }),
+      tap((recipes) => {
         this.recipeService.setRecipes(recipes);
-      });
+      })
+    );
   }
 }
