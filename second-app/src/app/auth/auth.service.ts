7362455d2de288a9359f426f57d5e622606bfbf8
nguyentu9 <huynhthanhtoan517@gmail.com>
2022-02-23 23:27:07 +0700

    20. 11 Improving Error Handling

--- second-app/src/app/auth/auth.service.ts
+++ second-app/src/app/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { catchError, throwError } from 'rxjs';
 
 interface AuthResponseData {
   kind: string;
@@ -9,20 +10,35 @@ interface AuthResponseData {
   expiresIn: string;
   localId: string;
 }
-
+// https://firebase.google.com/docs/reference/rest/auth
 // https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=
 @Injectable({ providedIn: 'root' })
 export class AuthService {
   constructor(private http: HttpClient) {}
 
   signup(email: string, password: string) {
-    return this.http.post<AuthResponseData>(
-      'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyCjhER7mezLjUz4UzBu75q0CvYz2BxMCCk',
-      {
-        email,
-        password,
-        returnSecureToken: true,
-      }
-    );
+    return this.http
+      .post<AuthResponseData>(
+        'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyCjhER7mezLjUz4UzBu75q0CvYz2BxMCCk',
+        {
+          email,
+          password,
+          returnSecureToken: true,
+        }
+      )
+      .pipe(
+        catchError((errorRes) => {
+          let errorMessage = 'An unknown error occurred!';
+          if (!errorRes.error || !errorRes.error.error) {
+            return throwError(() => errorMessage);
+          }
+
+          switch (errorRes.error.error.message) {
+            case 'EMAIL_EXISTS':
+              errorMessage = 'This email exists already';
+          }
+          return throwError(() => errorMessage);
+        })
+      );
   }
 }
