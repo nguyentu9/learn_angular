38ff018c2a19551bbbbe0b60a3944d074317b481
nguyentu9 <huynhthanhtoan517@gmail.com>
2022-02-24 14:44:44 +0700

    20. 19 Adding Auto Login

--- second-app/src/app/auth/auth.service.ts
+++ second-app/src/app/auth/auth.service.ts
@@ -81,6 +81,28 @@ export class AuthService {
     const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
     const user = new User(email, userId, token, expirationDate);
     this.user.next(user);
+    localStorage.setItem('userData', JSON.stringify(user));
+  }
+
+  autoLogin() {
+    const userData: {
+      email: string;
+      id: string;
+      _token: string;
+      _tokenExpirationDate: Date;
+    } = JSON.parse(localStorage.getItem('userData') || '');
+    if (!userData) return;
+
+    const loadedUser = new User(
+      userData.email,
+      userData.id,
+      userData._token,
+      new Date(userData._tokenExpirationDate)
+    );
+
+    if (loadedUser.token) {
+      this.user.next(loadedUser);
+    }
   }
 
   handleError(errorRes: HttpErrorResponse) {
