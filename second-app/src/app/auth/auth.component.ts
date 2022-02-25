0c058e0c2be54a513ca476707db9afda68ab07cc
nguyentu9 <huynhthanhtoan517@gmail.com>
2022-02-25 20:25:37 +0700

    21. 6 Creating a Component Programmatically

--- second-app/src/app/auth/auth.component.ts
+++ second-app/src/app/auth/auth.component.ts
@@ -1,8 +1,9 @@
-import { Component, ComponentFactoryResolver } from '@angular/core';
+import { Component, ComponentFactoryResolver, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AlertComponent } from '../shared/alert/alert.component';
+import { PlaceHolderDirective } from '../shared/placeholder/placeholder.directive';
 import { AuthResponseData, AuthService } from './auth.service';
 
 @Component({
@@ -13,6 +14,8 @@ export class AuthComponent {
   isLoginMode = true;
   isLoading = false;
   error: string | null = null;
+
+  @ViewChild(PlaceHolderDirective) alertHost!: PlaceHolderDirective;
 
   authObs!: Observable<AuthResponseData>;
 
@@ -61,5 +64,10 @@ export class AuthComponent {
   private showErrorAlert(message: string) {
     const alertCmpFactory =
       this.componentFactoryResolver.resolveComponentFactory(AlertComponent);
+
+    const hostViewContainerRef = this.alertHost.viewContainerRef;
+    hostViewContainerRef.clear();
+
+    hostViewContainerRef.createComponent(alertCmpFactory);
   }
 }
