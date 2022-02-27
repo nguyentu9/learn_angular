e1b4e4c16b645e94dbf8e27cf93f7e844b048fe1
nguyentu9 <huynhthanhtoan517@gmail.com>
2022-02-27 16:33:11 +0700

    22. 11 Adding an Auth Feature Module

--- second-app/src/app/app.module.ts
+++ second-app/src/app/app.module.ts
@@ -1,28 +1,26 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { CoreModule } from 'src/core.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AuthComponent } from './auth/auth.component';
+import { AuthModule } from './auth/auth.module';
 import { HeaderComponent } from './header/header.component';
 import { RecipesModule } from './recipes/recipes.module';
 import { ShareModule } from './shared/share.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, AuthComponent],
+  declarations: [AppComponent, HeaderComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule,
-    ReactiveFormsModule,
     HttpClientModule,
     RecipesModule,
     ShoppingListModule,
     ShareModule,
     CoreModule,
+    AuthModule,
   ],
   bootstrap: [AppComponent],
 })
